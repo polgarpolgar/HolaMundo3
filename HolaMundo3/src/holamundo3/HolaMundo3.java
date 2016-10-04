0ba7975ba97eb572b37cba80251831ac853ca66f
sardinilla <sardinilla@sardinilla-mentolada>
2016-10-05 01:06:09 +0200

    Empiezo de nuevo

--- HolaMundo3/src/holamundo3/HolaMundo3.java
+++ HolaMundo3/src/holamundo3/HolaMundo3.java
@@ -17,6 +17,7 @@ public class HolaMundo3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo 3");
+        System.out.println("");
     }
     
 }
