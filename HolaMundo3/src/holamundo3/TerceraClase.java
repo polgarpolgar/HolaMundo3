a964a2ccff9476f6d8fd61f1fc0f953ad1bb62d7
sardinilla <sardinilla@sardinilla-mentolada>
2016-10-05 02:00:33 +0200

    MOdificaciones a la primera clase

---
+++ HolaMundo3/src/holamundo3/TerceraClase.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo3;
+
+/**
+ *
+ * @author sardinilla
+ */
+public class TerceraClase {
+    String otraMas;
+}
